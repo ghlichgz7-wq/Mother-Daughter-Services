d26cef857b7bc7e97f8c9ea058455c726209807d
ghlichgz7-wq <281061607+ghlichgz7-wq@users.noreply.github.com>
2026-05-06 05:09:47 +1200

    Update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ const yearSpan = document.getElementById("year");
 if (yearSpan) {
     yearSpan.textContent = new Date().getFullYear();
 }
+
+// Changes active nav link decoration 
+document.addEventListener("DOMContentLoaded", () => {
+    const fullPath = window.location.pathname;
+    let currentFile = fullPath.split("/").pop();
+
+    if (currentFile === "" || currentFile === "index.html") {
+        currentFile = "index.html";
+    }
+
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    navLinks.forEach(link => {
+        const href = link.getAttribute("href");
+        const linkFile = href.split("/").pop();
+
+        if (currentFile === linkFile) {
+            link.classList.add("active");
+        }
+    });
+});
